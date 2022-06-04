3218e5e06c93d53e8b9697a650c97f95964c620a
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2022-06-04 13:41:43 -0300

    Adding Try Catch to UserController methods

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -2,39 +2,48 @@ import User from '../models/User.js'
 
 const UserController = {
     async create(req, res) {
-        const body = req.body
+        try {
+            const body = req.body
 
-        if (!body.name || !body.password || !req.body.email || !req.body.phone) {
-            return res.status(422).send({
+            if (!body.name || !body.password || !req.body.email || !req.body.phone) {
+                return res.status(422).send({
+                    success: false,
+                    message: "All fields are required"
+                })
+            }
+
+            if (body.name == "" || body.password == "" || req.body.email == "" || req.body.phone == "") {
+                return res.status(422).send({
+                    success: false,
+                    message: "All fields are required"
+                })
+            }
+
+            const user = {
+                name: body.name,
+                password: body.password,
+                email: body.email,
+                phone: body.phone
+            }
+
+            if (!await User.create(user)) {
+                return res.status(500).send({
+                    success: false,
+                    message: "An unexpected error occured"
+                })
+            }
+
+            res.status(201).send({
+                success: true
+            })
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({
                 success: false,
-                message: "All fields are required"
+                message: "An unexpected error occurred"
             })
         }
 
-        if (body.name == "" || body.password == "" || req.body.email == "" || req.body.phone == "") {
-            return res.status(422).send({
-                success: false,
-                message: "All fields are required"
-            })
-        }
-
-        const user = {
-            name: body.name,
-            password: body.password,
-            email: body.email,
-            phone: body.phone
-        }
-
-        if (!await User.create(user)) {
-            return res.status(500).send({
-                success: false,
-                message: "An unexpected error occured"
-            })
-        }
-
-        res.status(201).send({
-            success: true
-        })
     }
 }
 
