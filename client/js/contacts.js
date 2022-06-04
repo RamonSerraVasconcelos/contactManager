110b0460cb8bd6366c6ae38d19f07b7e2c5005f1
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2022-06-04 20:43:13 -0300

    Home and create contact pages Styling home page Create page and its functions

--- client/js/contacts.js
+++ client/js/contacts.js
@@ -7,3 +7,36 @@
             message(error.responseJSON.message)
         })
 })()
+
+$("#contactPic").click(() => {
+    $("#fileToUploadContactPic").click()
+})
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#contactPicture').attr('src', e.target.result)
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function validarImagem(field) {
+    documento = $("#" + field.id).val();
+    var upld = documento.split('.').pop();
+    if (upld == 'jpg' || upld == 'jpeg' || upld == 'png') {
+        const fileSize = field.files[0].size / 1024 / 1024; // in MiB
+        if (fileSize > 5) {
+            message("O arquivo não deve ser maior que 5MB.")
+            field.value = '';
+        }
+
+        readURL(field)
+    } else {
+        message("O documento deve estar nos formatos jpg, jpeg ou png.")
+        field.value = '';
+    }
+}
