7415820a3b167b01bbfcc8ee49b5a4a9da69d426
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2022-06-04 18:02:57 -0300

    Finishing registering page and functions

--- client/js/user.js
+++ client/js/user.js
@@ -13,3 +13,28 @@ $("#loginForm").submit(async (event) => {
             message(error.responseJSON.message)
         })
 })
+
+$('#registerForm').submit((event) => {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    const values = Object.fromEntries(data.entries())
+
+    if (values.name == "" || values.email == "" || values.phone == "" || values.password == "") {
+        message("Por favor preencha todos os campos.")
+        return
+    }
+
+    if (values.password != values.confirm_password) {
+        message("Por favor, garanta que as 2 senhas são iguais.")
+        return
+    }
+
+    request('/users/register', 'post', values)
+        .then((res) => {
+            message("Usuário cadastrado com sucesso.")
+            location.href = 'http://127.0.0.1:5500/client/index.html'
+        })
+        .catch((error) => {
+            message(error.responseJSON.message)
+        })
+})
