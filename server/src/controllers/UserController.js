41d176a8a830000453a65ad64407d1bb4fa5da82
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2022-06-06 19:34:19 -0300

    Sending email on user registration

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js'
+import Mailer from '../lib/mailer.js'
 
 const UserController = {
     async create(req, res) {
@@ -43,7 +44,22 @@ const UserController = {
                 })
             }
 
-            return res.status(201).send({
+            Mailer.sendMail({
+                to: body.email,
+                from: 'ramonserrav@hotmail.com',
+                subject: `Confirmação de inscrição - Agenda de Contatos`,
+                html: `
+                    <h1 style="text-align:center">Agenda de Contatos</h1>
+                    <br>
+                    <p>Olá ${body.name}, o seu usuário no website Agenda de Contato foi criado com sucesso.</p>
+                    <p style="text-align:center"><a href="http://127.0.0.1:5500/client/index.html" target="_blank" style="text-decoration:none;padding:5px;background:red;color:white;font-size:20px;font-weight:bold">LINK DE ACESSO</a></p>
+                    <div style="margin-top:200px;text-align:justify">
+                        <h2>Agenda de Contatos</h2>
+                    </div>
+                `
+            })
+
+            res.status(201).send({
                 message: "User created"
             })
         } catch (error) {
